3b73ee15301faa709183e84b14988030212bfd25
Johnny Tang <email.john.e@gmail.com>
2020-09-14 16:05:30 -0700

    add features to winning board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,3 @@
-//creates empty board
 const X = {
   score: 0,
   blocks: []
@@ -20,25 +19,24 @@ var winningBoard = [
   [3,5,7]
 ]
 
+var winningLine = [];
+
 const imageX = 'https://www.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_X_blue-512.png'
 const imageO = 'https://images.ctfassets.net/yr4qj72ki4ky/5gltqdfqQSQj1K9DPVMYWf/8e1ca3d632a440be80270f4952a878d0/hrhq-avatar.png'
 
+//creates empty board
 function createBoard() {
-
-  var blockId = 1
-  var board = document.getElementById('gameContainer')
+  document.getElementById('turn').innerHTML = `TURN: PLAYER ${turn===X ? 'X' : 'O'}`
+  let board = document.getElementById('gameContainer')
   board.innerHTML = '';
-  for (let i=0; i<9; i++) {
-    var block = document.createElement('span');
-    block.setAttribute('id', blockId);
+  for (let i=1; i<10; i++) {
+    let block = document.createElement('span');
+    block.setAttribute('id', i);
     block.setAttribute('class', 'block');
     block.addEventListener("click", makeMove);
     board.appendChild(block);
-    blockId++;
   }
 }
-createBoard()
-updateScore()
 
 //places X or O on clicked block, and checks for winner
 function makeMove() {
@@ -51,17 +49,35 @@ function makeMove() {
       turn.score++;
       updateScore();
       displayWinner();
+    } else if (checkDraw()) {
+      displayDraw();
     } else {
       turn = turn === X ? O : X
     }
+    document.getElementById('turn').innerHTML = `TURN: PLAYER ${turn===X ? 'X' : 'O'}`
   }
+}
+
+//shows Draw, button for new game
+function displayDraw() {
+  document.getElementById('winner').innerHTML = 'DRAW'
+  document.getElementById('display').style.display = 'block';
 }
 
 //shows winning line, displays winner, button for new game
 function displayWinner() {
+  //remove event listener from board, highlight winning line
+  let blocks = document.getElementsByClassName('block');
+  for (let block of blocks) {
+    block.removeEventListener("click", makeMove)
+  }
+  for (let i = 0; i < winningLine.length; i++) {
+    document.getElementById(winningLine[i].toString()).style.border = '5px blue dashed'
+  }
+
+  //show winner on bottom of page
   let winnerContainer = document.getElementById('winner');
   let winner = document.createElement('div');
-  let newGame = document.createElement('button')
   winnerContainer.innerHTML = `PLAYER ${turn === X ? 'X WINS' : 'O WINS'}`;
   winnerContainer.appendChild(winner);
   document.getElementById('display').style.display = 'block';
@@ -69,9 +85,9 @@ function displayWinner() {
 
 //updates score
 function updateScore() {
-  var scoreX = document.createElement('div');
+  let scoreX = document.createElement('div');
   scoreX.setAttribute('class', 'score');
-  var scoreO = document.createElement('div');
+  let scoreO = document.createElement('div');
   scoreO.setAttribute('class', 'score');
   scoreX.innerHTML = `Player X: ${X.score}`;
   scoreO.innerHTML = `Player O: ${O.score}`;
@@ -80,12 +96,23 @@ function updateScore() {
   document.getElementById('scoreboard').append(scoreO);
 }
 
+//detects draw
+function checkDraw() {
+  for(let i = 1; i <= 9; i++) {
+    if (!document.getElementById(i.toString()).style.backgroundImage) {
+      return false;
+    }
+  }
+  console.log('draw')
+  return true;
+}
 
 //detects winner
 function checkWinner(player) {
   console.log(player.blocks)
   for(let i = 0; i < winningBoard.length; i++) {
     if (winningBoard[i].every((val) => player.blocks.includes(val))) {
+      winningLine = winningBoard[i]
       return true;
     }
   }
@@ -99,3 +126,6 @@ function newGame() {
   turn = X;
   document.getElementById('display').style.display = 'none'
 }
+
+createBoard()
+updateScore()
